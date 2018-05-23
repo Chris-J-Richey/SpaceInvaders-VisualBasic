c24d097230db55e595dd8395c6b8db4fe02c8e7a
Chris Richey <chris.j.richey@gmail.com>
2018-05-22 22:17:07 -0700

    upload
    all files

---
+++ SpaceInvaders3.0/frmMain.Designer.vb
@@ -0,0 +1,380 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmMain))
+        Me.picBullet6 = New System.Windows.Forms.PictureBox()
+        Me.picBullet5 = New System.Windows.Forms.PictureBox()
+        Me.picBullet4 = New System.Windows.Forms.PictureBox()
+        Me.picIn5 = New System.Windows.Forms.PictureBox()
+        Me.picIn6 = New System.Windows.Forms.PictureBox()
+        Me.PicIn4 = New System.Windows.Forms.PictureBox()
+        Me.picBullet3 = New System.Windows.Forms.PictureBox()
+        Me.picBullet2 = New System.Windows.Forms.PictureBox()
+        Me.picBullet1 = New System.Windows.Forms.PictureBox()
+        Me.picYouBullet = New System.Windows.Forms.PictureBox()
+        Me.sidebottom = New System.Windows.Forms.Label()
+        Me.lblEnd = New System.Windows.Forms.Label()
+        Me.picYou = New System.Windows.Forms.PictureBox()
+        Me.picIn2 = New System.Windows.Forms.PictureBox()
+        Me.picIn3 = New System.Windows.Forms.PictureBox()
+        Me.picIn1 = New System.Windows.Forms.PictureBox()
+        Me.picBlank = New System.Windows.Forms.PictureBox()
+        Me.EnemyShoot1 = New System.Windows.Forms.Timer(Me.components)
+        Me.EnemyShoot2 = New System.Windows.Forms.Timer(Me.components)
+        Me.EnemyShoot3 = New System.Windows.Forms.Timer(Me.components)
+        Me.EnemyShoot4 = New System.Windows.Forms.Timer(Me.components)
+        Me.EnemyShoot6 = New System.Windows.Forms.Timer(Me.components)
+        Me.EnemyShoot5 = New System.Windows.Forms.Timer(Me.components)
+        Me.EnemyShootStart = New System.Windows.Forms.Timer(Me.components)
+        Me.Shoot = New System.Windows.Forms.Timer(Me.components)
+        Me.InvadersMove = New System.Windows.Forms.Timer(Me.components)
+        Me.sideright = New System.Windows.Forms.Label()
+        Me.sideleft = New System.Windows.Forms.Label()
+        CType(Me.picBullet6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBullet5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBullet4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picIn5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picIn6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PicIn4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBullet3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBullet2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBullet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picYouBullet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picYou, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picIn2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picIn3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picIn1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picBlank, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'picBullet6
+        '
+        Me.picBullet6.Image = CType(resources.GetObject("picBullet6.Image"), System.Drawing.Image)
+        Me.picBullet6.Location = New System.Drawing.Point(137, 47)
+        Me.picBullet6.Name = "picBullet6"
+        Me.picBullet6.Size = New System.Drawing.Size(21, 40)
+        Me.picBullet6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picBullet6.TabIndex = 133
+        Me.picBullet6.TabStop = False
+        Me.picBullet6.Visible = False
+        '
+        'picBullet5
+        '
+        Me.picBullet5.Image = CType(resources.GetObject("picBullet5.Image"), System.Drawing.Image)
+        Me.picBullet5.Location = New System.Drawing.Point(78, 47)
+        Me.picBullet5.Name = "picBullet5"
+        Me.picBullet5.Size = New System.Drawing.Size(21, 40)
+        Me.picBullet5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picBullet5.TabIndex = 132
+        Me.picBullet5.TabStop = False
+        Me.picBullet5.Visible = False
+        '
+        'picBullet4
+        '
+        Me.picBullet4.Image = CType(resources.GetObject("picBullet4.Image"), System.Drawing.Image)
+        Me.picBullet4.Location = New System.Drawing.Point(20, 47)
+        Me.picBullet4.Name = "picBullet4"
+        Me.picBullet4.Size = New System.Drawing.Size(21, 40)
+        Me.picBullet4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picBullet4.TabIndex = 131
+        Me.picBullet4.TabStop = False
+        Me.picBullet4.Visible = False
+        '
+        'picIn5
+        '
+        Me.picIn5.Image = CType(resources.GetObject("picIn5.Image"), System.Drawing.Image)
+        Me.picIn5.Location = New System.Drawing.Point(63, 47)
+        Me.picIn5.Name = "picIn5"
+        Me.picIn5.Size = New System.Drawing.Size(53, 40)
+        Me.picIn5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picIn5.TabIndex = 130
+        Me.picIn5.TabStop = False
+        '
+        'picIn6
+        '
+        Me.picIn6.Image = CType(resources.GetObject("picIn6.Image"), System.Drawing.Image)
+        Me.picIn6.Location = New System.Drawing.Point(122, 47)
+        Me.picIn6.Name = "picIn6"
+        Me.picIn6.Size = New System.Drawing.Size(53, 40)
+        Me.picIn6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picIn6.TabIndex = 129
+        Me.picIn6.TabStop = False
+        '
+        'PicIn4
+        '
+        Me.PicIn4.Image = CType(resources.GetObject("PicIn4.Image"), System.Drawing.Image)
+        Me.PicIn4.Location = New System.Drawing.Point(4, 47)
+        Me.PicIn4.Name = "PicIn4"
+        Me.PicIn4.Size = New System.Drawing.Size(53, 40)
+        Me.PicIn4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PicIn4.TabIndex = 128
+        Me.PicIn4.TabStop = False
+        '
+        'picBullet3
+        '
+        Me.picBullet3.Image = CType(resources.GetObject("picBullet3.Image"), System.Drawing.Image)
+        Me.picBullet3.Location = New System.Drawing.Point(137, 1)
+        Me.picBullet3.Name = "picBullet3"
+        Me.picBullet3.Size = New System.Drawing.Size(21, 40)
+        Me.picBullet3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picBullet3.TabIndex = 127
+        Me.picBullet3.TabStop = False
+        Me.picBullet3.Visible = False
+        '
+        'picBullet2
+        '
+        Me.picBullet2.Image = CType(resources.GetObject("picBullet2.Image"), System.Drawing.Image)
+        Me.picBullet2.Location = New System.Drawing.Point(78, 1)
+        Me.picBullet2.Name = "picBullet2"
+        Me.picBullet2.Size = New System.Drawing.Size(21, 40)
+        Me.picBullet2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picBullet2.TabIndex = 126
+        Me.picBullet2.TabStop = False
+        Me.picBullet2.Visible = False
+        '
+        'picBullet1
+        '
+        Me.picBullet1.Image = CType(resources.GetObject("picBullet1.Image"), System.Drawing.Image)
+        Me.picBullet1.Location = New System.Drawing.Point(20, 1)
+        Me.picBullet1.Name = "picBullet1"
+        Me.picBullet1.Size = New System.Drawing.Size(21, 40)
+        Me.picBullet1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picBullet1.TabIndex = 125
+        Me.picBullet1.TabStop = False
+        Me.picBullet1.Visible = False
+        '
+        'picYouBullet
+        '
+        Me.picYouBullet.Image = CType(resources.GetObject("picYouBullet.Image"), System.Drawing.Image)
+        Me.picYouBullet.Location = New System.Drawing.Point(170, 228)
+        Me.picYouBullet.Name = "picYouBullet"
+        Me.picYouBullet.Size = New System.Drawing.Size(21, 40)
+        Me.picYouBullet.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picYouBullet.TabIndex = 124
+        Me.picYouBullet.TabStop = False
+        Me.picYouBullet.Visible = False
+        '
+        'sidebottom
+        '
+        Me.sidebottom.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.sidebottom.Location = New System.Drawing.Point(-100, 245)
+        Me.sidebottom.Name = "sidebottom"
+        Me.sidebottom.Size = New System.Drawing.Size(975, 1)
+        Me.sidebottom.TabIndex = 122
+        '
+        'lblEnd
+        '
+        Me.lblEnd.BackColor = System.Drawing.Color.Transparent
+        Me.lblEnd.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblEnd.ForeColor = System.Drawing.Color.White
+        Me.lblEnd.Location = New System.Drawing.Point(57, 92)
+        Me.lblEnd.Name = "lblEnd"
+        Me.lblEnd.Size = New System.Drawing.Size(269, 69)
+        Me.lblEnd.TabIndex = 121
+        Me.lblEnd.Text = "ykhgvbfds"
+        Me.lblEnd.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.lblEnd.Visible = False
+        '
+        'picYou
+        '
+        Me.picYou.Image = CType(resources.GetObject("picYou.Image"), System.Drawing.Image)
+        Me.picYou.Location = New System.Drawing.Point(150, 264)
+        Me.picYou.Name = "picYou"
+        Me.picYou.Size = New System.Drawing.Size(60, 40)
+        Me.picYou.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picYou.TabIndex = 120
+        Me.picYou.TabStop = False
+        '
+        'picIn2
+        '
+        Me.picIn2.Image = CType(resources.GetObject("picIn2.Image"), System.Drawing.Image)
+        Me.picIn2.Location = New System.Drawing.Point(63, 1)
+        Me.picIn2.Name = "picIn2"
+        Me.picIn2.Size = New System.Drawing.Size(53, 40)
+        Me.picIn2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picIn2.TabIndex = 119
+        Me.picIn2.TabStop = False
+        '
+        'picIn3
+        '
+        Me.picIn3.Image = CType(resources.GetObject("picIn3.Image"), System.Drawing.Image)
+        Me.picIn3.Location = New System.Drawing.Point(122, 1)
+        Me.picIn3.Name = "picIn3"
+        Me.picIn3.Size = New System.Drawing.Size(53, 40)
+        Me.picIn3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picIn3.TabIndex = 118
+        Me.picIn3.TabStop = False
+        '
+        'picIn1
+        '
+        Me.picIn1.Image = CType(resources.GetObject("picIn1.Image"), System.Drawing.Image)
+        Me.picIn1.Location = New System.Drawing.Point(4, 1)
+        Me.picIn1.Name = "picIn1"
+        Me.picIn1.Size = New System.Drawing.Size(53, 40)
+        Me.picIn1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picIn1.TabIndex = 117
+        Me.picIn1.TabStop = False
+        '
+        'picBlank
+        '
+        Me.picBlank.BackColor = System.Drawing.Color.Black
+        Me.picBlank.Location = New System.Drawing.Point(306, -20)
+        Me.picBlank.Name = "picBlank"
+        Me.picBlank.Size = New System.Drawing.Size(53, 40)
+        Me.picBlank.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picBlank.TabIndex = 123
+        Me.picBlank.TabStop = False
+        '
+        'EnemyShoot1
+        '
+        Me.EnemyShoot1.Interval = 35
+        '
+        'EnemyShoot2
+        '
+        Me.EnemyShoot2.Interval = 35
+        '
+        'EnemyShoot3
+        '
+        Me.EnemyShoot3.Interval = 35
+        '
+        'EnemyShoot4
+        '
+        Me.EnemyShoot4.Interval = 35
+        '
+        'EnemyShoot6
+        '
+        Me.EnemyShoot6.Interval = 35
+        '
+        'EnemyShoot5
+        '
+        Me.EnemyShoot5.Interval = 35
+        '
+        'EnemyShootStart
+        '
+        Me.EnemyShootStart.Enabled = True
+        Me.EnemyShootStart.Interval = 20
+        '
+        'Shoot
+        '
+        Me.Shoot.Interval = 20
+        '
+        'InvadersMove
+        '
+        Me.InvadersMove.Enabled = True
+        Me.InvadersMove.Interval = 80
+        '
+        'sideright
+        '
+        Me.sideright.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.sideright.Location = New System.Drawing.Point(399, -22)
+        Me.sideright.Name = "sideright"
+        Me.sideright.Size = New System.Drawing.Size(2, 500)
+        Me.sideright.TabIndex = 134
+        Me.sideright.Text = "Label2"
+        '
+        'sideleft
+        '
+        Me.sideleft.BackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.sideleft.Location = New System.Drawing.Point(-2, -74)
+        Me.sideleft.Name = "sideleft"
+        Me.sideleft.Size = New System.Drawing.Size(2, 500)
+        Me.sideleft.TabIndex = 135
+        Me.sideleft.Text = "Label3"
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(400, 325)
+        Me.Controls.Add(Me.sideleft)
+        Me.Controls.Add(Me.sideright)
+        Me.Controls.Add(Me.picBullet6)
+        Me.Controls.Add(Me.picBullet5)
+        Me.Controls.Add(Me.picBullet4)
+        Me.Controls.Add(Me.picIn5)
+        Me.Controls.Add(Me.picIn6)
+        Me.Controls.Add(Me.PicIn4)
+        Me.Controls.Add(Me.picBullet3)
+        Me.Controls.Add(Me.picBullet2)
+        Me.Controls.Add(Me.picBullet1)
+        Me.Controls.Add(Me.picYouBullet)
+        Me.Controls.Add(Me.sidebottom)
+        Me.Controls.Add(Me.lblEnd)
+        Me.Controls.Add(Me.picYou)
+        Me.Controls.Add(Me.picIn2)
+        Me.Controls.Add(Me.picIn3)
+        Me.Controls.Add(Me.picIn1)
+        Me.Controls.Add(Me.picBlank)
+        Me.Name = "frmMain"
+        Me.Text = "Form1"
+        CType(Me.picBullet6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBullet5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBullet4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picIn5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picIn6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PicIn4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBullet3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBullet2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBullet1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picYouBullet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picYou, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picIn2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picIn3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picIn1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picBlank, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents picBullet6 As System.Windows.Forms.PictureBox
+    Friend WithEvents picBullet5 As System.Windows.Forms.PictureBox
+    Friend WithEvents picBullet4 As System.Windows.Forms.PictureBox
+    Friend WithEvents picIn5 As System.Windows.Forms.PictureBox
+    Friend WithEvents picIn6 As System.Windows.Forms.PictureBox
+    Friend WithEvents PicIn4 As System.Windows.Forms.PictureBox
+    Friend WithEvents picBullet3 As System.Windows.Forms.PictureBox
+    Friend WithEvents picBullet2 As System.Windows.Forms.PictureBox
+    Friend WithEvents picBullet1 As System.Windows.Forms.PictureBox
+    Friend WithEvents picYouBullet As System.Windows.Forms.PictureBox
+    Friend WithEvents sidebottom As System.Windows.Forms.Label
+    Friend WithEvents lblEnd As System.Windows.Forms.Label
+    Friend WithEvents picYou As System.Windows.Forms.PictureBox
+    Friend WithEvents picIn2 As System.Windows.Forms.PictureBox
+    Friend WithEvents picIn3 As System.Windows.Forms.PictureBox
+    Friend WithEvents picIn1 As System.Windows.Forms.PictureBox
+    Friend WithEvents picBlank As System.Windows.Forms.PictureBox
+    Friend WithEvents EnemyShoot1 As System.Windows.Forms.Timer
+    Friend WithEvents EnemyShoot2 As System.Windows.Forms.Timer
+    Friend WithEvents EnemyShoot3 As System.Windows.Forms.Timer
+    Friend WithEvents EnemyShoot4 As System.Windows.Forms.Timer
+    Friend WithEvents EnemyShoot6 As System.Windows.Forms.Timer
+    Friend WithEvents EnemyShoot5 As System.Windows.Forms.Timer
+    Friend WithEvents EnemyShootStart As System.Windows.Forms.Timer
+    Friend WithEvents Shoot As System.Windows.Forms.Timer
+    Friend WithEvents InvadersMove As System.Windows.Forms.Timer
+    Friend WithEvents sideright As System.Windows.Forms.Label
+    Friend WithEvents sideleft As System.Windows.Forms.Label
+
+End Class
---
+++ SpaceInvaders3.0/frmMain.vb
@@ -0,0 +1,220 @@
+﻿
+'Name Chris Richey
+'Period 2
+'date 3/14/11
+'discription Space Invaders game
+
+Public Class frmMain
+
+    Private x As Integer = 10
+    Private invader(6) As PictureBox
+    Private bullet(6) As PictureBox
+    Private EnemyShoot(6) As Timer
+
+    Private Sub frmMain_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        invader(1) = picIn1
+        invader(2) = picIn2
+        invader(3) = picIn3
+        invader(4) = PicIn4
+        invader(5) = picIn5
+        invader(6) = picIn6
+        bullet(1) = picBullet1
+        bullet(2) = picBullet2
+        bullet(3) = picBullet3
+        bullet(4) = picBullet4
+        bullet(5) = picBullet5
+        bullet(6) = picBullet6
+        EnemyShoot(1) = EnemyShoot1
+        EnemyShoot(2) = EnemyShoot2
+        EnemyShoot(3) = EnemyShoot3
+        EnemyShoot(4) = EnemyShoot4
+        EnemyShoot(5) = EnemyShoot5
+        EnemyShoot(6) = EnemyShoot6
+        My.Computer.Audio.Play(My.Resources.spaceinvaders1, AudioPlayMode.Background)
+    End Sub
+
+    Private Sub frmMain_KeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles MyBase.KeyDown
+        Dim x = picYou.Left
+        Dim key As KeyEventArgs
+
+        key = e
+        'moves you left and right
+        If InvadersMove.Enabled = True Then
+            If picYou.Left >= -1 And picYou.Right <= Me.Width Then
+                Select Case key.KeyCode
+                    Case 37
+                        x -= 10
+                    Case 39
+                        x += 10
+                End Select
+            End If
+            If picYou.Left <= 0 Then
+                x += 10
+            End If
+            If picYou.Right >= Me.Width Then
+                x -= 10
+            End If
+            If key.KeyCode = 32 Then
+                'shoots the bullet
+                Shoot.Enabled = True
+                My.Computer.Audio.Play(My.Resources.shoot, AudioPlayMode.Background)
+            End If
+            picYou.SetBounds(x, picYou.Top, picYou.Width, picYou.Height)
+            If Shoot.Enabled = False Then
+                picYouBullet.SetBounds(picYou.Left + 20, picYou.Top - 36, picYouBullet.Width, picYouBullet.Height)
+            End If
+        End If
+        If key.KeyCode = 27 Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub Shoot_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Shoot.Tick
+        Dim p As Integer = 1
+        picYouBullet.Visible = True
+        picYouBullet.Top -= 10
+        If picYouBullet.Top <= 0 Then
+            picYouBullet.Top = picYou.Top - 36
+            picYouBullet.Left = picYou.Left + 20
+            picYouBullet.Visible = False
+            Shoot.Enabled = False
+        End If
+        For p = 1 To 6
+            'sees if you hit them
+            If picYouBullet.Bounds.IntersectsWith(invader(p).Bounds) Then
+                invader(p).Top = -300
+                Shoot.Enabled = False
+                picYouBullet.Top = picYou.Top - 36
+                picYouBullet.Left = picYou.Left + 20
+                picYouBullet.Visible = False
+                My.Computer.Audio.Play(My.Resources.explosion, AudioPlayMode.Background)
+            End If
+        Next
+    End Sub
+
+    Private Sub EnemyShootStart_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles EnemyShootStart.Tick
+        'send an enemy shoot it random number is right
+        Dim random As Integer
+        Dim h As Integer = 1
+        Dim g As Integer = 1
+        Randomize()
+        random = Rnd() * 600
+        If random = 50 Then
+            If picIn1.Top >= -1 Then
+                bullet(1).SetBounds(picIn1.Left + 20, picIn1.Top, bullet(1).Width, bullet(1).Height)
+                EnemyShoot1.Enabled = True
+            End If
+        ElseIf random = 150 Then
+            If picIn2.Top >= -1 Then
+                bullet(2).SetBounds(picIn2.Left + 20, picIn2.Top, bullet(2).Width, bullet(2).Height)
+                EnemyShoot2.Enabled = True
+            End If
+        ElseIf random = 250 Then
+            If picIn3.Top >= -1 Then
+                bullet(3).SetBounds(picIn3.Left + 20, picIn3.Top, bullet(3).Width, bullet(3).Height)
+                EnemyShoot3.Enabled = True
+            End If
+        ElseIf random = 350 Then
+            If PicIn4.Top >= -1 Then
+                bullet(4).SetBounds(PicIn4.Left + 20, PicIn4.Top, bullet(4).Width, bullet(4).Height)
+                EnemyShoot4.Enabled = True
+            End If
+        ElseIf random = 450 Then
+            If picIn5.Top >= -1 Then
+                bullet(5).SetBounds(picIn5.Left + 20, picIn5.Top, bullet(5).Width, bullet(5).Height)
+                EnemyShoot5.Enabled = True
+            End If
+        ElseIf random = 550 Then
+            If picIn6.Top >= -1 Then
+                bullet(6).SetBounds(picIn6.Left + 20, picIn6.Top, bullet(6).Width, bullet(6).Height)
+                EnemyShoot6.Enabled = True
+            End If
+        End If
+
+        For h = 1 To 6
+            'checks if it they hit you
+            If bullet(h).Bounds.IntersectsWith(picYou.Bounds) Then
+                bullet(h).Visible = False
+                EnemyShoot(h).Enabled = False
+                lblEnd.Visible = True
+                lblEnd.Text = "You Lost"
+                InvadersMove.Enabled = False
+                EnemyShootStart.Enabled = False
+                My.Computer.Audio.Play(My.Resources.explosion, AudioPlayMode.Background)
+            End If
+        Next
+        For g = 1 To 6
+            'checks if it they hit the bottom
+            If invader(g).Bounds.IntersectsWith(sidebottom.Bounds) Then
+                bullet(g).Visible = False
+                EnemyShoot(g).Enabled = False
+                lblEnd.Visible = True
+                lblEnd.Text = "You Lost"
+                InvadersMove.Enabled = False
+                EnemyShootStart.Enabled = False
+            End If
+        Next
+    End Sub
+
+    Private Sub EnemyShoot1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles EnemyShoot1.Tick
+        bullet(1).Visible = True
+        bullet(1).Top += 10
+    End Sub
+
+    Private Sub EnemyShoot2_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles EnemyShoot2.Tick
+        bullet(2).Visible = True
+        bullet(2).Top += 10
+    End Sub
+
+    Private Sub EnemyShoot3_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles EnemyShoot3.Tick
+        bullet(3).Visible = True
+        bullet(3).Top += 10
+    End Sub
+
+    Private Sub EnemyShoot4_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles EnemyShoot4.Tick
+        bullet(4).Visible = True
+        bullet(4).Top += 10
+    End Sub
+
+    Private Sub EnemyShoot5_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles EnemyShoot5.Tick
+        bullet(5).Visible = True
+        bullet(5).Top += 10
+    End Sub
+
+    Private Sub EnemyShoot6_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles EnemyShoot6.Tick
+        bullet(6).Visible = True
+        bullet(6).Top += 10
+    End Sub
+
+    Private Sub InvadersMove_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles InvadersMove.Tick
+        Dim l As Integer = 1
+        Dim w As Integer = 1
+        Dim a As Integer = 1
+        For w = 1 To 6
+            If invader(w).Bounds.IntersectsWith(sideright.Bounds) Then
+                x = -10
+            ElseIf invader(w).Bounds.IntersectsWith(sideleft.Bounds) Then
+                x = 10
+            End If
+        Next
+        For a = 1 To 6
+            If invader(a).Bounds.IntersectsWith(sideright.Bounds) Or invader(a).Bounds.IntersectsWith(sideleft.Bounds) Then
+                invader(1).Top += 15
+                invader(2).Top += 15
+                invader(3).Top += 15
+                invader(4).Top += 15
+                invader(5).Top += 15
+                invader(6).Top += 15
+            End If
+        Next
+        For l = 1 To 6
+            invader(l).Left += x
+        Next
+        If picIn1.Top <= -1 And picIn2.Top <= -1 And picIn3.Top <= -1 And PicIn4.Top <= -1 And picIn5.Top <= -1 And picIn6.Top <= -1 Then
+            lblEnd.Visible = True
+            lblEnd.Text = "You Won!"
+            InvadersMove.Enabled = False
+            EnemyShootStart.Enabled = False
+        End If
+    End Sub
+End Class
